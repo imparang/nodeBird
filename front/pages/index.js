9b0a14e4151e75734b73dc445ceef170d125cf0a
imparang <jeongjaeyn@gmail.com>
2021-12-01 16:23:46 +0900

    feat: Create index.js: Home page

---
+++ front/pages/index.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import AppLayout from '../components/AppLayout'
+
+const index = () => {
+  return (
+    <AppLayout>
+      <div>
+        Hello, Next
+      </div>
+    </AppLayout>
+  )
+}
+
+export default index
