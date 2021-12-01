e603592b1fe2abe558b6af461263595c46cb470b
imparang <jeongjaeyn@gmail.com>
2021-12-01 16:24:16 +0900

    feat: Create signup.js: Sign up page

---
+++ front/pages/signup.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import AppLayout from '../components/AppLayout';
+import Head from 'next/head';
+
+const Signup = () => {
+  return (
+    <>
+      <Head>
+        <title>회원가입 | NodeBird</title>
+      </Head>
+      <AppLayout>
+        회원가입
+      </AppLayout>
+    </>
+  );
+};
+
+export default Signup;
