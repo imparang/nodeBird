551cbf8b0aa825e6edd70a84e6229106d4ec0e8c
imparang <jeongjaeyn@gmail.com>
2021-12-04 04:03:46 +0900

    feat: Create Profile page

--- front/pages/profile.js
+++ front/pages/profile.js
@@ -1,18 +1,25 @@
-import React from 'react';
-import AppLayout from '../components/AppLayout';
-import Head from 'next/head';
+import React from 'react'
+import AppLayout from '../components/AppLayout'
+import Head from 'next/head'
+import FollowList from '../components/FollowList'
+import FollowerList from '../components/FollowerList'
+import NicknameEditForm from '../components/NicknameEditForm'
 
 const Profile = () => {
+  const followerList = [{nickname: 'one'}, {nickname: 'two'}, {nickname: 'three'}]
+  const followingList = [{nickname: 'four'}, {nickname: 'five'}, {nickname: 'six'}]
   return (
     <>
       <Head>
         <title>내 프로필 | NodeBird</title>
       </Head>
       <AppLayout>
-          내 프로필
+          <NicknameEditForm />
+          <FollowList header="팔로잉 목록" data={followingList} />
+          <FollowerList header="팔로워 목록" data={followerList} />
       </AppLayout>
     </>
-  );
-};
+  )
+}
 
-export default Profile;
+export default Profile
