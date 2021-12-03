e10a2953283245cbf0ffda6e02f08be42d95331d
imparang <jeongjaeyn@gmail.com>
2021-12-04 04:06:55 +0900

    feat: Create UserProfile component

---
+++ front/components/UserProfile.js
@@ -0,0 +1,29 @@
+import React, { useCallback } from 'react'
+import { Avatar, Button, Card } from 'antd'
+import { useDispatch } from 'react-redux'
+import { logoutAction } from '../reducers/user'
+
+
+const UserProfile = () => {
+  const dispatch = useDispatch()
+  const onLogOut = useCallback(() => {
+    dispatch(logoutAction())
+  })
+  return (
+    <Card 
+      actions={[
+        <div key="twit">트윗<br/>0</div>,
+        <div key="followings">팔로잉<br/>0</div>,
+        <div key="followers">팔로워<br/>0</div>
+      ]}
+    >
+      <Card.Meta 
+        avatar={<Avatar>Bleu</Avatar>}
+        title="Parang"
+      />
+      <Button onClick={onLogOut}>로그아웃</Button>
+    </Card>
+  )
+}
+
+export default UserProfile
