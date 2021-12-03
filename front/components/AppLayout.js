269682fc1662f0a787e7c5f8d41084e553a0a8ab
imparang <jeongjaeyn@gmail.com>
2021-12-04 04:01:51 +0900

    feat: Create AppLayout component

--- front/components/AppLayout.js
+++ front/components/AppLayout.js
@@ -1,24 +1,74 @@
 import React from 'react'
+import { useSelector } from 'react-redux'
 import PropTypes from 'prop-types'
 import Link from 'next/link'
-import { Menu } from 'antd'
+import { Input, Menu, Row, Col } from 'antd'
+import UserProfile from './UserProfile'
+import LoginForm from './LoginForm'
+import styled, { createGlobalStyle } from 'styled-components'
 
+const SearchInput = styled(Input.Search)`
+  vertical-align: middle;
+`
+
+const Global = createGlobalStyle`
+  .ant-row {
+    margin-right: 0 !important;
+    margin-left: 0 !important;
+  }
+
+  .ant-col:first-child {
+    padding-left: 0 !important;
+  }
+
+  .ant-col:last-child {
+    padding-rignt: 0 !important;
+  }
+`
 
 const AppLayout = ({ children }) => {
+  const { isLoggedIn } = useSelector(state => state.user)
+
   return (
     <div>
+      <Global />
       <Menu mode="horizontal">
-        <Menu.Item>
-          <Link href="/"><a>노드버드</a></Link>
+        <Menu.Item key="home">
+          <Link href="/">
+            <a>노드버드</a>
+          </Link>
         </Menu.Item>
-        <Menu.Item>
-          <Link href="/profile"><a>프로필</a></Link>
+        <Menu.Item key="profile">
+          <Link href="/profile">
+            <a>프로필</a>
+          </Link>
         </Menu.Item>
-        <Menu.Item>
-          <Link href="/signup"><a>회원가입</a></Link>
+        <Menu.Item key="search">
+          <SearchInput enterButton />
+        </Menu.Item>
+        <Menu.Item key="sigup">
+          <Link href="/signup">
+            <a>회원가입</a>
+          </Link>
         </Menu.Item>
       </Menu>
-      {children}
+      <Row gutter={8}>
+        <Col xs={24} md={6}>
+          {isLoggedIn ? <UserProfile /> : <LoginForm />}
+        </Col>
+        <Col xs={24} md={12}>
+          {children}
+        </Col>
+        <Col xs={24} md={6}>
+          <a
+            href="https://google.com"
+            rel="noreferrer noopener"
+            target="_blank"
+          >
+            구글가기
+          </a>
+        </Col>
+      </Row>
     </div>
   )
 }
