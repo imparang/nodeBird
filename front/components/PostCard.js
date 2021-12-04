6ff514d2f10d520b5e52118b1c837981a03bc062
imparang <jeongjaeyn@gmail.com>
2021-12-04 20:21:56 +0900

    feat: Create PostCard component

--- front/components/PostCard.js
+++ front/components/PostCard.js
@@ -1,10 +1,17 @@
 import { Avatar, Button, Card, Comment, Popover, List } from 'antd'
 import React, { useCallback, useState } from 'react'
-import { EllipsisOutlined, HeartOutlined, MessageOutlined, RetweetOutlined, HeartTwoTone } from '@ant-design/icons'
+import {
+  EllipsisOutlined,
+  HeartOutlined,
+  MessageOutlined,
+  RetweetOutlined,
+  HeartTwoTone
+} from '@ant-design/icons'
 import { useSelector } from 'react-redux'
 import PropTypes from 'prop-types'
 import PostImages from './PostImages'
 import CommentForm from './CommentForm'
+import PostCardContent from './PostCardContent'
 
 const PostCard = ({ post }) => {
   const id = useSelector(state => state.user.me?.id)
@@ -21,44 +28,62 @@ const PostCard = ({ post }) => {
 
   return (
     <div style={{ marginBottom: '20px' }}>
-      <Card cover={post.Images[0] && <PostImages images={post.Images} />}
+      <Card
+        cover={post.Images[0] && <PostImages images={post.Images} />}
         actions={[
-          <RetweetOutlined key='retweet' />,
-          liked 
-            ? <HeartTwoTone twoToneColor='#eb2f96' key='heart' onClick={onToggleLike} /> 
-            : <HeartOutlined key='heart' onClick={onToggleLike} />,
-          <MessageOutlined  key='comment' onClick={onToggleComment}/>,
-          <Popover key='more' content={(
-            <Button.Group>
-              {id && id === post.User.id 
-              ? (
+          <RetweetOutlined key="retweet" />,
+          liked ? (
+            <HeartTwoTone
+              twoToneColor="#eb2f96"
+              key="heart"
+              onClick={onToggleLike}
+            />
+          ) : (
+            <HeartOutlined key="heart" onClick={onToggleLike} />
+          ),
+          <MessageOutlined key="comment" onClick={onToggleComment} />,
+          <Popover
+            key="more"
+            content={
+              <Button.Group>
+                {id && id === post.User.id ? (
                   <>
                     <Button>수정</Button>
                     <Button type="danger">삭제</Button>
                   </>
-                ) 
-              : (
-                <Button>신고</Button>
-              )}
-            </Button.Group>
-          )}>
+                ) : (
+                  <Button>신고</Button>
+                )}
+              </Button.Group>
+            }
+          >
             <EllipsisOutlined />
           </Popover>
         ]}
       >
-        <Card.Meta description={post.content} title={post.User.nickname} avatar={<Avatar>{post.User.nickname[0]}</Avatar>} />
+        <Card.Meta
+          description={<PostCardContent postData={post.content} />}
+          title={post.User.nickname}
+          avatar={<Avatar>{post.User.nickname[0]}</Avatar>}
+        />
       </Card>
       {commentFormOpend && (
         <div>
           <CommentForm post={post} />
-          <List header={`${post.Comments.length}개의 댓글`}
-          itemLayout="horizontal"
-          dataSource={post.Comments}
-          renderItem={item => (
-            <li>
-              <Comment author={item.User.nickname} avatar={<Avatar>{item.User.nickname[0]}</Avatar>} content={item.content} />
-            </li>
-          )} />
+          <List
+            header={`${post.Comments.length}개의 댓글`}
+            itemLayout="horizontal"
+            dataSource={post.Comments}
+            renderItem={item => (
+              <li>
+                <Comment
+                  author={item.User.nickname}
+                  avatar={<Avatar>{item.User.nickname[0]}</Avatar>}
+                  content={item.content}
+                />
+              </li>
+            )}
+          />
         </div>
       )}
     </div>
