4d0077f477ad6858cb68aaa9b98648f450d1aef3
imparang <jeongjaeyn@gmail.com>
2021-12-04 20:19:44 +0900

    feat: Add a common saga middleware

--- front/store/configureStore.js
+++ front/store/configureStore.js
@@ -1,20 +1,31 @@
 import { createWrapper } from 'next-redux-wrapper'
 import { applyMiddleware, compose, createStore } from 'redux'
 import reducer from '../reducers/index'
+import rootSaga from '../sagas'
 import { composeWithDevTools } from 'redux-devtools-extension'
+import createSagaMiddleware from 'redux-saga'
 
+const loggerMiddleware =
+  ({ dispatch, getState }) =>
+  next =>
+  action => {
+    console.log(action)
+    return next(action)
+  }
 const configureStore = () => {
-  const middlewares = []
-  const enhancer = process.env.Node_ENV === 'production'
-    ? compose(applyMiddleware(...middlewares))
-    : composeWithDevTools(applyMiddleware(...middlewares))
-
+  const sagaMiddleware = createSagaMiddleware()
+  const middlewares = [sagaMiddleware, loggerMiddleware]
+  const enhancer =
+    process.env.Node_ENV === 'production'
+      ? compose(applyMiddleware(...middlewares))
+      : composeWithDevTools(applyMiddleware(...middlewares))
 
   const store = createStore(reducer, enhancer)
+  store.sagaTask = sagaMiddleware.run(rootSaga)
   return store
 }
 
-const wrapper = createWrapper(configureStore, { 
+const wrapper = createWrapper(configureStore, {
   debug: process.env.Node_ENV === 'development'
 })
 
