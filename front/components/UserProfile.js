cc66d4d831ba43edd3c198d69d7b31f1a71d9b5f
imparang <jeongjaeyn@gmail.com>
2021-12-04 20:23:31 +0900

    feat: Apply pending data to login, logout logic

--- front/components/UserProfile.js
+++ front/components/UserProfile.js
@@ -1,27 +1,38 @@
 import React, { useCallback } from 'react'
 import { Avatar, Button, Card } from 'antd'
-import { useDispatch } from 'react-redux'
-import { logoutAction } from '../reducers/user'
-
+import { useDispatch, useSelector } from 'react-redux'
+import { logoutRequestAction } from '../reducers/user'
 
 const UserProfile = () => {
+  const { me, isLoggingOut } = useSelector(state => state.user)
   const dispatch = useDispatch()
   const onLogOut = useCallback(() => {
-    dispatch(logoutAction())
+    dispatch(logoutRequestAction())
   })
   return (
-    <Card 
+    <Card
       actions={[
-        <div key="twit">트윗<br/>0</div>,
-        <div key="followings">팔로잉<br/>0</div>,
-        <div key="followers">팔로워<br/>0</div>
+        <div key="twit">
+          트윗
+          <br />0
+        </div>,
+        <div key="followings">
+          팔로잉
+          <br />0
+        </div>,
+        <div key="followers">
+          팔로워
+          <br />0
+        </div>
       ]}
     >
-      <Card.Meta 
-        avatar={<Avatar>Bleu</Avatar>}
-        title="Parang"
+      <Card.Meta
+        avatar={<Avatar>{me.nickname[0]}</Avatar>}
+        title={me.nickname}
       />
-      <Button onClick={onLogOut}>로그아웃</Button>
+      <Button onClick={onLogOut} loading={isLoggingOut}>
+        로그아웃
+      </Button>
     </Card>
   )
 }
