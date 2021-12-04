13ac635a51ad9dd813265b6cbf3420052bf58d18
imparang <jeongjaeyn@gmail.com>
2021-12-04 20:21:25 +0900

    feat: Implement redux reducers to seperate saga generators

--- front/reducers/user.js
+++ front/reducers/user.js
@@ -1,21 +1,37 @@
-const LOG_IN = 'LOG_IN'
-const LOG_OUT = 'LOG_OUT'
+export const LOG_IN_REQUEST = 'LOG_IN_REQUEST'
+export const LOG_IN_SUCCESS = 'LOG_IN_SUCCESS'
+export const LOG_IN_FAILURE = 'LOG_IN_FAILURE'
+export const LOG_OUT_REQUEST = 'LOG_OUT_REQUEST'
+export const LOG_OUT_SUCCESS = 'LOG_OUT_SUCCESS'
+export const LOG_OUT_FAILURE = 'LOG_OUT_FAILURE'
 
-export const loginAction = data => {
+// thunk
+// export const loginAction = data => (dispatch, getState) => {
+//   const state = getState()
+//   dispatch(loginRequestAction())
+//   axios
+//     .post('')
+//     .then(res => dispatch(loginSuccessAction, res.data))
+//     .catch(error => dispatch(loginFailureAction, error))
+// }
+
+export const loginRequestAction = data => {
   return {
-    type: LOG_IN,
+    type: LOG_IN_REQUEST,
     data
   }
 }
 
-export const logoutAction = () => {
+export const logoutRequestAction = () => {
   return {
-    type: LOG_OUT
+    type: LOG_OUT_REQUEST
   }
 }
 
 export const initialState = {
+  isLoggingIn: false, // 로그인 시도중
   isLoggedIn: false,
+  isLoggingOut: false, // 로그아웃 시도중
   me: null,
   signUpData: {},
   loginData: {}
@@ -23,18 +39,42 @@ export const initialState = {
 
 const reducer = (state = initialState, action) => {
   switch (action.type) {
-    case LOG_IN:
+    case LOG_IN_REQUEST:
       return {
         ...state,
-        isLoggedIn: true,
-        me: action.data
+        isLoggingIn: true
       }
-      case LOG_OUT:
-        return {
-          ...state,
-          isLoggedIn: false,
-          me: null
-        }
+    case LOG_IN_SUCCESS:
+      return {
+        ...state,
+        isLoggingIn: false,
+        isLoggedIn: true,
+        me: { ...action.data, nickname: 'noze' }
+      }
+    case LOG_IN_FAILURE:
+      return {
+        ...state,
+        isLoggingIn: false,
+        isLoggedIn: false
+      }
+    case LOG_OUT_REQUEST:
+      return {
+        ...state,
+        isLoggingOut: true
+      }
+    case LOG_OUT_SUCCESS:
+      return {
+        ...state,
+        isLoggingOut: false,
+        isLoggedIn: false,
+        me: null
+      }
+    case LOG_OUT_FAILURE:
+      return {
+        ...state,
+        isLoggingOut: false,
+        isLoggedIn: false
+      }
     default:
       return state
   }
