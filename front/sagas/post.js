ecdeedca00141a2b2438e6a47e32474d169286b0
imparang <jeongjaeyn@gmail.com>
2021-12-06 01:55:21 +0900

    feat: Add to add post and comment logic with axios in redux-saga

--- front/sagas/post.js
+++ front/sagas/post.js
@@ -1,29 +1,60 @@
 import axios from 'axios'
 import { all, fork, delay, put, takeLatest } from 'redux-saga/effects'
+import {
+  ADD_COMMENT_FAILURE,
+  ADD_COMMENT_REQUEST,
+  ADD_COMMENT_SUCCESS,
+  ADD_POST_FAILURE,
+  ADD_POST_REQUEST,
+  ADD_POST_SUCCESS
+} from '../reducers/types'
 
-function addPostAPI(data) {
-  return axios.post('/api/post', data)
-}
+// function addPostAPI(data) {
+//   return axios.post('/api/post', data)
+// }
 
 function* addPost(action) {
   try {
     // const result = yield call(addPostAPI, action.data)
     yield delay(1000)
     yield put({
-      type: 'ADD_POST_SUCCESS'
+      type: ADD_POST_SUCCESS
     })
   } catch (error) {
     yield put({
-      type: 'ADD_POST_FAILUTRE',
+      type: ADD_POST_FAILURE,
+      data: error.response.data
+    })
+  }
+}
+
+// function addCommentAPI(data) {
+//   return axios.post('/api/comment', data)
+// }
+
+function* addComment(action) {
+  try {
+    // const result = yield call(addCommentAPI, action.data)
+    yield delay(1000)
+    yield put({
+      type: ADD_COMMENT_SUCCESS
+    })
+  } catch (error) {
+    yield put({
+      type: ADD_COMMENT_FAILURE,
       data: error.response.data
     })
   }
 }
 
 function* watchAddPost() {
-  yield takeLatest('ADD_POST_REQUEST', addPost)
+  yield takeLatest(ADD_POST_REQUEST, addPost)
+}
+
+function* watchAddComment() {
+  yield takeLatest(ADD_COMMENT_REQUEST, addComment)
 }
 
 export default function* postSaga() {
-  yield all([fork(watchAddPost)])
+  yield all([fork(watchAddPost), fork(watchAddComment)])
 }
