7317d6c887efccc359d3b84f3157eb6d10fa197b
imparang <jeongjaeyn@gmail.com>
2021-12-06 01:56:02 +0900

    feat: Add state about loading logic and seperate action types

--- front/reducers/user.js
+++ front/reducers/user.js
@@ -1,19 +1,14 @@
-export const LOG_IN_REQUEST = 'LOG_IN_REQUEST'
-export const LOG_IN_SUCCESS = 'LOG_IN_SUCCESS'
-export const LOG_IN_FAILURE = 'LOG_IN_FAILURE'
-export const LOG_OUT_REQUEST = 'LOG_OUT_REQUEST'
-export const LOG_OUT_SUCCESS = 'LOG_OUT_SUCCESS'
-export const LOG_OUT_FAILURE = 'LOG_OUT_FAILURE'
-
-// thunk
-// export const loginAction = data => (dispatch, getState) => {
-//   const state = getState()
-//   dispatch(loginRequestAction())
-//   axios
-//     .post('')
-//     .then(res => dispatch(loginSuccessAction, res.data))
-//     .catch(error => dispatch(loginFailureAction, error))
-// }
+import {
+  LOG_IN_FAILURE,
+  LOG_IN_REQUEST,
+  LOG_IN_SUCCESS,
+  LOG_OUT_FAILURE,
+  LOG_OUT_REQUEST,
+  LOG_OUT_SUCCESS,
+  SIGN_UP_FAILURE,
+  SIGN_UP_REQUEST,
+  SIGN_UP_SUCCESS
+} from './types'
 
 export const loginRequestAction = data => {
   return {
@@ -29,51 +24,89 @@ export const logoutRequestAction = () => {
 }
 
 export const initialState = {
-  isLoggingIn: false, // 로그인 시도중
-  isLoggedIn: false,
-  isLoggingOut: false, // 로그아웃 시도중
+  logInLoading: false, // 로그인 시도중
+  logInDone: false,
+  logInError: null,
+  logOutLoading: false, // 로그아웃 시도중
+  logOutDone: false,
+  logOutError: null,
+  signUpLoading: false, // 회원가입 시도중
+  signUpDone: false,
+  signUpError: null,
   me: null,
   signUpData: {},
   loginData: {}
 }
+
+const dummyUser = data => ({
+  ...data,
+  nickname: 'noze',
+  id: 1,
+  Posts: [],
+  Followings: [],
+  Follwers: []
+})
 
 const reducer = (state = initialState, action) => {
   switch (action.type) {
     case LOG_IN_REQUEST:
       return {
         ...state,
-        isLoggingIn: true
+        logInLoading: true,
+        logInDone: false,
+        logInError: null
       }
     case LOG_IN_SUCCESS:
       return {
         ...state,
-        isLoggingIn: false,
-        isLoggedIn: true,
-        me: { ...action.data, nickname: 'noze' }
+        logInLoading: false,
+        logInDone: true,
+        me: dummyUser(action.data)
       }
     case LOG_IN_FAILURE:
       return {
         ...state,
-        isLoggingIn: false,
-        isLoggedIn: false
+        logInLoading: false,
+        logInError: action.error
       }
     case LOG_OUT_REQUEST:
       return {
         ...state,
-        isLoggingOut: true
+        logOutLoading: true,
+        logOutDone: false,
+        logOutError: null
       }
     case LOG_OUT_SUCCESS:
       return {
         ...state,
-        isLoggingOut: false,
-        isLoggedIn: false,
+        logOutLoading: false,
+        logOutDone: true,
         me: null
       }
     case LOG_OUT_FAILURE:
       return {
         ...state,
-        isLoggingOut: false,
-        isLoggedIn: false
+        logOutLoading: false,
+        logOutError: action.error
+      }
+    case SIGN_UP_REQUEST:
+      return {
+        ...state,
+        signUpLoading: true,
+        signUpDone: false,
+        signUpError: null
+      }
+    case SIGN_UP_SUCCESS:
+      return {
+        ...state,
+        signUpLoading: false,
+        signUpDone: true
+      }
+    case SIGN_UP_FAILURE:
+      return {
+        ...state,
+        signUpLoading: false,
+        signUpError: action.error
       }
     default:
       return state
@@ -81,3 +114,13 @@ const reducer = (state = initialState, action) => {
 }
 
 export default reducer
+
+// thunk
+// export const loginAction = data => (dispatch, getState) => {
+//   const state = getState()
+//   dispatch(loginRequestAction())
+//   axios
+//     .post('')
+//     .then(res => dispatch(loginSuccessAction, res.data))
+//     .catch(error => dispatch(loginFailureAction, error))
+// }
