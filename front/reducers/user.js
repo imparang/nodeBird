e37dd8061dd83409e7487c7a3f389bb00facc4e4
imparang <jeongjaeyn@gmail.com>
2021-12-10 15:11:40 +0900

    feat: Append follow action types and reducers

--- front/reducers/user.js
+++ front/reducers/user.js
@@ -13,7 +13,13 @@ import {
   REMOVE_POST_OF_ME,
   SIGN_UP_FAILURE,
   SIGN_UP_REQUEST,
-  SIGN_UP_SUCCESS
+  SIGN_UP_SUCCESS,
+  FOLLOW_REQUEST,
+  FOLLOW_SUCCESS,
+  FOLLOW_FAILURE,
+  UNFOLLOW_REQUEST,
+  UNFOLLOW_SUCCESS,
+  UNFOLLOW_FAILURE
 } from './types'
 
 export const loginRequestAction = data => {
@@ -30,6 +36,12 @@ export const logoutRequestAction = () => {
 }
 
 export const initialState = {
+  followLoading: false, // 팔로우 시도중
+  followDone: false,
+  followError: null,
+  unfollowLoading: false, // 언팔로우 시도중
+  unfollowDone: false,
+  unfollowError: null,
   logInLoading: false, // 로그인 시도중
   logInDone: false,
   logInError: null,
@@ -63,6 +75,36 @@ const dummyUser = data => ({
 const reducer = (state = initialState, action) =>
   produce(state, draft => {
     switch (action.type) {
+      case FOLLOW_REQUEST:
+        draft.followLoading = true
+        draft.followDone = false
+        draft.followError = null
+        break
+      case FOLLOW_SUCCESS:
+        draft.followLoading = false
+        draft.followDone = true
+        draft.me.Followings.push({ id: action.data })
+        break
+      case FOLLOW_FAILURE:
+        draft.followLoading = false
+        draft.followError = action.error
+        break
+      case UNFOLLOW_REQUEST:
+        draft.unfollowLoading = true
+        draft.unfollowDone = false
+        draft.unfollowError = null
+        break
+      case UNFOLLOW_SUCCESS:
+        draft.unfollowLoading = false
+        draft.unfollowDone = true
+        draft.me.Followings = draft.me.Followings.filter(
+          v => v.id !== action.data
+        )
+        break
+      case UNFOLLOW_FAILURE:
+        draft.unfollowLoading = false
+        draft.unfollowError = action.error
+        break
       case LOG_IN_REQUEST:
         draft.logInLoading = true
         draft.logInDone = false
