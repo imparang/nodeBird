3b8c679b530c78e23a53618dddac06a17dab3c7b
imparang <jeongjaeyn@gmail.com>
2022-01-18 17:44:30 +0900

    feat: Set cors of  credentials option is true

--- back/app.js
+++ back/app.js
@@ -1,23 +1,3 @@
-const express = require('express')
-const cors = require('cors')
-
-const userRouter = require('./routes/user')
-const postRouter = require('./routes/post')
-const db = require('./models')
-const passportConfig = require('./passport')
-const session = require('express-session')
-const passport = require('passport')
-const cookieParser = require('cookie-parser')
-const dotenv = require('dotenv')
-dotenv.config()
-
-db.sequelize.sync()
-  .then(() => {
-    console.log('db 연결성공')
-  })
-  .catch((error) =>console.log(error))
-
-passportConfig()
 /*
 get -> 가져오기
 post -> 생성하기
@@ -28,50 +8,55 @@ options -> 찔러보기 (서버야, 요청 보낼 수 있어?)
 head -> 헤더만 가져오기
 */
 
-const app = express()
+const express = require('express');
+const cors = require('cors');
+const session = require('express-session');
+const cookieParser = require('cookie-parser');
+const passport = require('passport');
+const dotenv = require('dotenv');
+const morgan = require('morgan');
+const path = require('path');
+
+const postRouter = require('./routes/post');
+const postsRouter = require('./routes/posts');
+const userRouter = require('./routes/user');
+const db = require('./models');
+const passportConfig = require('./passport');
+
+dotenv.config();
+const app = express();
+db.sequelize.sync()
+  .then(() => {
+    console.log('db 연결 성공');
+  })
+  .catch(console.error);
+passportConfig();
+
+app.use(morgan('dev'));
 app.use(cors({
-  origin: '*',
-  credentials: false
-}))
-app.use(express.json())
-app.use(express.urlencoded({ extended: true }))
-app.use(cookieParser(process.env.COOKIE_SECRET))
+  origin: 'http://localhost:3000',
+  credentials: true,
+}));
+app.use('/', express.static(path.join(__dirname, 'uploads')));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser(process.env.COOKIE_SECRET));
 app.use(session({
   saveUninitialized: false,
   resave: false,
-  secret: process.env.COOKIE_SECRET
-}))
-app.use(passport.initialize())
-app.use(passport.session())
+  secret: process.env.COOKIE_SECRET,
+}));
+app.use(passport.initialize());
+app.use(passport.session());
 
 app.get('/', (req, res) => {
-  res.send('hello express')
-})
-
-app.get('/api', (req, res) => {
-  res.send('hello api')
-})
-
-app.get('/api/posts', (req, res) => {
-  res.json([
-    {
-      id: 1,
-      content: 'hello1'
-    },
-    {
-      id: 2,
-      content: 'hello2'
-    },
-    {
-      id: 3,
-      content: 'hello3'
-    }
-  ])
-})
-
-app.use('/post', postRouter)
-app.use('/user', userRouter)
+  res.send('hello express');
+});
+// API는 다른 서비스가 내 서비스의 기능을 실행할 수 있게 열어둔 창구
+app.use('/posts', postsRouter);
+app.use('/post', postRouter);
+app.use('/user', userRouter);
 
 app.listen(5001, () => {
-  console.log('서버 실행 중')
-})
+  console.log('서버 실행 중!');
+});
