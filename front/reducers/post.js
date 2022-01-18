020e2bd4a4dcc7dafac5fc1ebff4e4411e3f8e40
imparang <jeongjaeyn@gmail.com>
2022-01-18 20:08:14 +0900

    feat: Add post reducer

--- front/reducers/post.js
+++ front/reducers/post.js
@@ -1,10 +1,8 @@
-import shortId from 'shortid'
 import produce from 'immer'
-import faker from 'faker'
 import {
-  LOAD_POST_FAILURE,
-  LOAD_POST_REQUEST,
-  LOAD_POST_SUCCESS,
+  LOAD_POSTS_FAILURE,
+  LOAD_POSTS_REQUEST,
+  LOAD_POSTS_SUCCESS,
   ADD_POST_FAILURE,
   ADD_POST_REQUEST,
   ADD_POST_SUCCESS,
@@ -13,17 +11,29 @@ import {
   ADD_COMMENT_SUCCESS,
   REMOVE_POST_REQUEST,
   REMOVE_POST_SUCCESS,
-  REMOVE_POST_FAILURE
+  REMOVE_POST_FAILURE,
+  LIKE_POST_REQUEST,
+  LIKE_POST_SUCCESS,
+  LIKE_POST_FAILURE,
+  UNLIKE_POST_REQUEST,
+  UNLIKE_POST_SUCCESS,
+  UNLIKE_POST_FAILURE,
+  UPLOAD_IMAGES_REQUEST,
+  UPLOAD_IMAGES_SUCCESS,
+  UPLOAD_IMAGES_FAILURE,
+  REMOVE_IMAGE,
+  RETWEET_REQUEST,
+  RETWEET_SUCCESS,
+  RETWEET_FAILURE
 } from './types'
-import shortid from 'shortid'
 
 const initialState = {
   mainPosts: [],
   imagePaths: [],
   hasMorePost: true,
-  loadPostLoading: false,
-  loadPostDone: false,
-  loadPostError: null,
+  loadPostsLoading: false,
+  loadPostsDone: false,
+  loadPostsError: null,
   addPostLoading: false,
   addPostDone: false,
   addPostError: null,
@@ -32,41 +42,25 @@ const initialState = {
   addCommentError: null,
   removePostLoading: false,
   removePostDone: false,
-  removePostError: null
+  removePostError: null,
+  likePostLoading: false,
+  likePostDone: false,
+  likePostError: null,
+  unLikePostLoading: false,
+  unLikePostDone: false,
+  unLikePostError: null,
+  uploadImagesLoading: false,
+  uploadImagesDone: false,
+  uploadImagesError: null,
+  retweetLoading: false,
+  retweetDone: false,
+  retweetError: null
 }
 
-export const generateDummyPost = number =>
-  Array(number)
-    .fill()
-    .map(() => ({
-      id: shortid.generate(),
-      Images: [
-        {
-          src: faker.image.image()
-        }
-      ],
-      Comments: [
-        {
-          User: {
-            id: shortid.generate(),
-            nickname: faker.name.findName()
-          },
-          content: faker.lorem.paragraph()
-        }
-      ],
-      User: {
-        id: shortid.generate(),
-        nickname: faker.name.findName()
-      },
-      content: faker.lorem.paragraph()
-    }))
-
-initialState.mainPosts = initialState.mainPosts.concat(generateDummyPost(10))
-
-export const addPost = data => {
+export const addPost = text => {
   return {
     type: ADD_POST_REQUEST,
-    data
+    data: text
   }
 }
 
@@ -84,44 +78,57 @@ export const removePost = data => {
   }
 }
 
-const dummyPost = data => ({
-  id: data.id,
-  content: data.content,
-  User: {
-    id: 1,
-    nickname: 'monika'
-  },
-  Images: [],
-  Comments: []
-})
-
-const dummyComment = data => ({
-  id: shortId.generate(),
-  content: data,
-  User: {
-    id: 1,
-    nickname: 'aiki'
-  }
-})
-
 // 이전 상태를 액션을 통해 다음 상태로 만들어내는 함수(불변성은 지키면서)
 const reducer = (state = initialState, action) => {
   return produce(state, draft => {
     switch (action.type) {
-      case LOAD_POST_REQUEST:
-        draft.loadPostLoading = true
-        draft.loadPostDone = false
-        draft.loadPostError = null
+      case RETWEET_REQUEST:
+        draft.retweetLoading = true
+        draft.retweetDone = false
+        draft.retweetError = null
         break
-      case LOAD_POST_SUCCESS:
-        draft.loadPostLoading = false
-        draft.loadPostDone = true
+      case RETWEET_SUCCESS: {
+        draft.retweetLoading = false
+        draft.retweetDone = true
+        draft.mainPosts.unshift(action.data)
+        break
+      }
+      case RETWEET_FAILURE:
+        draft.retweetLoading = false
+        draft.retweetError = action.error
+        break
+      case REMOVE_IMAGE:
+        draft.imagePaths = draft.imagePaths.filter((v, i) => i !== action.data)
+        break
+      case UPLOAD_IMAGES_REQUEST:
+        draft.uploadImagesLoading = true
+        draft.uploadImagesDone = false
+        draft.uploadImagesError = null
+        break
+      case UPLOAD_IMAGES_SUCCESS: {
+        draft.imagePaths = action.data
+        draft.uploadImagesLoading = false
+        draft.uploadImagesDone = true
+        break
+      }
+      case UPLOAD_IMAGES_FAILURE:
+        draft.uploadImagesLoading = false
+        draft.uploadImagesError = action.error
+        break
+      case LOAD_POSTS_REQUEST:
+        draft.loadPostsLoading = true
+        draft.loadPostsDone = false
+        draft.loadPostsError = null
+        break
+      case LOAD_POSTS_SUCCESS:
+        draft.loadPostsLoading = false
+        draft.loadPostsDone = true
         draft.mainPosts = action.data.concat(draft.mainPosts)
         draft.hasMorePost = draft.mainPosts.length < 50
         break
-      case LOAD_POST_FAILURE:
-        draft.loadPostLoading = false
-        draft.loadPostError = action.error
+      case LOAD_POSTS_FAILURE:
+        draft.loadPostsLoading = false
+        draft.loadPostsError = action.error
         break
       case ADD_POST_REQUEST:
         draft.addPostLoading = true
@@ -131,7 +138,8 @@ const reducer = (state = initialState, action) => {
       case ADD_POST_SUCCESS:
         draft.addPostLoading = false
         draft.addPostDone = true
-        draft.mainPosts.unshift(dummyPost(action.data))
+        draft.mainPosts.unshift(action.data)
+        draft.imagePaths = []
         break
       case ADD_POST_FAILURE:
         draft.addPostLoading = false
@@ -156,8 +164,8 @@ const reducer = (state = initialState, action) => {
         //   addCommentLoading: false,
         //   addCommentDone: true
         // }
-        const post = draft.mainPosts.find(v => v.id === action.data.postId)
-        post.Comments.unshift(dummyComment(action.data.content))
+        const post = draft.mainPosts.find(v => v.id === action.data.PostId)
+        post.Comments.unshift(action.data)
         draft.addCommentLoading = false
         draft.addCommentDone = true
         break
@@ -174,11 +182,45 @@ const reducer = (state = initialState, action) => {
       case REMOVE_POST_SUCCESS:
         draft.removePostLoading = false
         draft.removePostDone = true
-        draft.mainPosts = state.mainPosts.filter(v => v.id !== action.data)
+        draft.mainPosts = state.mainPosts.filter(
+          v => v.id !== action.data.PostId
+        )
         break
       case REMOVE_POST_FAILURE:
         draft.removePostLoading = false
         draft.removePostError = action.error
+        break
+      case LIKE_POST_REQUEST:
+        draft.likePostLoading = true
+        draft.likePostDone = false
+        draft.likePostError = null
+        break
+      case LIKE_POST_SUCCESS: {
+        const post = draft.mainPosts.find(v => v.id === action.data.PostId)
+        post.Likers.push({ id: action.data.UserId })
+        draft.likePostLoading = false
+        draft.likePostDone = true
+        break
+      }
+      case LIKE_POST_FAILURE:
+        draft.likePostLoading = false
+        draft.likePostError = action.error
+        break
+      case UNLIKE_POST_REQUEST:
+        draft.unLikePostLoading = true
+        draft.unLikePostDone = false
+        draft.unLikePostError = null
+        break
+      case UNLIKE_POST_SUCCESS: {
+        const post = draft.mainPosts.find(v => v.id === action.data.PostId)
+        post.Likers = post.Likers.filter(v => v.id !== action.data.UserId)
+        draft.unLikePostLoading = false
+        draft.unLikePostDone = true
+        break
+      }
+      case UNLIKE_POST_FAILURE:
+        draft.unLikePostLoading = false
+        draft.unLikePostError = action.error
         break
       default:
         return state
